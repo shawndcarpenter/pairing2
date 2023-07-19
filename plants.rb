d7d7eaab0db3c044fc9ba3b998e68dca066c0268
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-07-19 10:30:47 -0600

    Add variables and conditional statement in plants.rb

---
+++ plants.rb
@@ -0,0 +1,9 @@
+gets_sunshine = false
+ounces_of_water = 5
+if gets_sunshine == true && ounces_of_water == 10
+    p "Your plant will live"
+elsif gets_sunshine == true || ounces_of_water == 10
+    p "Your plant needs attention"
+else
+    p "Your plant will die"
+end
