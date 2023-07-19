73bc975e5ad763c0e4f5c399fa1a10eef722b9dc
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-07-19 10:21:12 -0600

    Complete friends

---
+++ friends.rb
@@ -0,0 +1,7 @@
+arrays = ["joe", "dave", "steve", "charlie", "cynthia"]
+p arrays[2]
+p arrays[4]
+p arrays[0]
+places_to_visit = ["rome", "china", "el salvador", "new york", "los angeles"]
+p places_to_visit.pop
+p places_to_visit
